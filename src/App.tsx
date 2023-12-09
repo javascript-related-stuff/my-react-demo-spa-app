9c8619aaad0ced76cdcaa0abac2aacea5e01a2ea
Priyanshu Belwal <priyanshubelwalpb@gmail.com>
2023-12-09 20:05:55 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from 'my-react-demo-component-library'
+import './App.scss'
+
+function App() {
+
+  return (
+    <>
+      <h1>My React Demo SPA App</h1>
+      <Button className="">Button From Library</Button>
+    </>
+  )
+}
+
+export default App
